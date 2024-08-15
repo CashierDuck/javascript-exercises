79e827fd3a5fec38f5969664697f1225723d09d9
Mike Rubio <150881306+CashierDuck@users.noreply.github.com>
2024-08-15 02:45:08 -0500

    Attempt Ex 4 and Finish Exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...manyMoreArgs) {
+
+    for(const arg of manyMoreArgs){
+        if (arg != Number){
+            for(let i = 0; i < arr.length; i++){
+                if(arg === arr[i]){
+                    arr.splice(arg,arg+1);
+                }
+            }
+        } else if (arg == Number){
+            arr.splice(arg,arg+1);
+        } else {
+            break;
+        }
+    }
+
+    return arr;
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secNum) {
+    let sum = 0;
 
+    if(typeof(firstNum) != "number" || typeof(secNum) != "number" || firstNum < 0 || secNum < 0 || firstNum%1 != 0 || secNum%1 != 0){
+        return "ERROR";
+    }
+
+    if(firstNum > secNum){
+        let temp = secNum;
+        secNum = firstNum;
+        firstNum = temp;
+    }
+
+    for(let i = secNum; i >= firstNum; i--){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
